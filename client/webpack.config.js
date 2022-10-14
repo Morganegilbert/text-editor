18a78c0fd19609cb0f0b5ebb693b8fab686f0158
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-10-13 21:53:03 -0400

    re-adding dependencies and npm

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,9 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const WorkboxPlugin = require('workbox-webpack-plugin');
+new WorkboxPlugin.GenerateSW()
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
